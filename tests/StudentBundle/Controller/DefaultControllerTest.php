3356f1453c65a37582823ca97709221290fbc319
Bartłomiej Malarz <bartlomiej.malarz@gmail.com>
2018-07-18 22:41:28 +0200

    after testing and minor css tricks

--- tests/StudentBundle/Controller/DefaultControllerTest.php
+++ tests/StudentBundle/Controller/DefaultControllerTest.php
@@ -2,16 +2,41 @@
 
 namespace StudentBundle\Tests\Controller;
 
+use StudentBundle\Controller\DefaultController;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * @throws \Exception
+     */
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/list');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Liczba")')->count()
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testMainView() {
+        $client = static::createClient();
+        $crawler = $client->request('GET','/2018/7');
+        $this->assertGreaterThan(0, $crawler->filter('h2')->count());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testRedirectToDate() {
+        $client = static::createClient();
+        //$crawler = $client->request('GET','/');
+        $this->assertTrue($client->getResponse()->isRedirect());
     }
 }
